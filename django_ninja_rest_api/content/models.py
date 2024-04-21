55dbac3f8765d0b5aa28be5091f87be383d7cff3
TheMechanicalBeing <tornike.tsulukidze@gmail.com>
2024-04-21 14:19:21 +0400

    Initiate article model into content

--- django_ninja_rest_api/content/models.py
+++ django_ninja_rest_api/content/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from tinymce.models import HTMLField
 from treebeard.mp_tree import MP_Node
+
 
 
 class Category(MP_Node):
     name = models.CharField(max_length=250)
-    logo = models.ImageField(upload_to='uploads/logos')
+    logo = models.ImageField(upload_to='uploads/logos', blank=True, null=True)
 
     node_order_by = ['name']
 
@@ -23,3 +26,20 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=500)
+    description = HTMLField()
+    post_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey('users.CustomUser', on_delete=models.RESTRICT, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT, blank=True, null=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    image = models.ImageField(upload_to='uploads/articles', blank=True, null=True)
+    publish = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = 'Articles'
+
+    def __str__(self):
+        return self.title
