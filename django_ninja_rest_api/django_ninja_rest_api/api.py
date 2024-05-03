00bab96501332e119ba114e9133ba99a9530e5d2
TheMechanicalBeing <tornike.tsulukidze@gmail.com>
2024-05-03 21:36:43 +0400

    Initiate skeleton of API service's GET methods

--- django_ninja_rest_api/django_ninja_rest_api/api.py
+++ django_ninja_rest_api/django_ninja_rest_api/api.py
@@ -3,6 +3,16 @@ from ninja import NinjaAPI
 api = NinjaAPI()
 
 
-@api.get('/helloworld')
-def first(request):
-    return {'Hello': 'World!'}
+@api.get('/blocks')
+def blocks_get(request, category: str = None, tag: str = None, block_id: int = None):
+    return {'API Service': 'BLOCKS'}
+
+
+@api.get('/menus')
+def menus_get(request, category: str = None, menu_id: int = None):
+    return {'API Service': 'MENUS'}
+
+
+@api.get('/categories')
+def categories_get(request, category_id: int = None):
+    return {'API Service': 'CATEGORIES'}
